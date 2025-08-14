1c751e6d134e20b91ac5e1e1ac1b4419a2492169
Dark Boy <45736317+dark-boy81@users.noreply.github.com>
2025-08-14 21:30:09 +0330

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,25 @@
+from http.server import BaseHTTPRequestHandler
+import json
+import os
+
+class handler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        if self.path == '/webhook':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            
+            # Print update to Vercel logs (critical for debugging)
+            print("Received update:", json.loads(post_data))
+            
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'ok')
+        else:
+            self.send_response(404)
+            
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'Webhook is ready!')
